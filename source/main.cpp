dbc92d70fff28140d16fe45ec0b0cff01de150af
RookieKuch09 <rookiekuch09@gmail.com>
2025-06-18 21:40:08 +0930

    Event system showcase

--- source/main.cpp
+++ source/main.cpp
@@ -1,22 +1,109 @@
 #include <mosaic/application/application.hpp>
 #include <mosaic/application/instance.hpp>
 
+#include <mosaic/ecs/view.hpp>
+
 #include <mosaic/api/main.hpp>
 
-struct Component
+struct Health
 {
-    int a;
-    int b;
+    float Value;
 };
 
-void System(Mosaic::Resources& resources)
+struct Speed
 {
-    auto method = [&resources](Mosaic::ECS::Entity entity, Component& component)
-    {
-        resources.Console.Log<Mosaic::Debug::Console::LogSeverity::Success>("Result of adding Component values 'a' and 'b' on entity {}: {}", entity.ID, component.a + component.b);
-    };
+    float Value;
+};
 
-    resources.ECSManager.ForEach<Component>(method);
+struct Position
+{
+    float X, Y;
+};
+
+struct Weapon
+{
+    float Damage;
+};
+
+struct TimeTracker
+{
+    int Steps;
+    int Max;
+};
+
+struct DamageEvent
+{
+    Mosaic::Entity Target;
+
+    float Amount;
+};
+
+void OnDamage(Mosaic::Resources& resources, const DamageEvent& event, Mosaic::ECSView<Health> view)
+{
+    auto& console = resources.Console;
+
+    for (auto [entity, health] : view)
+    {
+        if (entity == event.Target)
+        {
+            health.Value -= event.Amount;
+
+            console.Log<Mosaic::Console::LogSeverity::Notice>("Entity {} took damage: {}. New health: {}", entity.ID, event.Amount, health.Value);
+        }
+    }
+}
+
+void AttackSystem(Mosaic::Resources& resources)
+{
+    auto& ecsManager = resources.ECSManager;
+    auto& eventManager = resources.EventManager;
+    auto& console = resources.Console;
+
+    auto healthView = ecsManager.QueryView<Health>();
+    auto weaponView = ecsManager.QueryView<Weapon>();
+
+    for (auto [entity, health] : healthView)
+    {
+        console.Log<Mosaic::Console::LogSeverity::Notice>("Health of entity {}: {}", entity.ID, health.Value);
+    }
+
+    for (auto [attackerEntity, weapon] : weaponView)
+    {
+        for (auto [targetEntity, health] : healthView)
+        {
+            if (targetEntity != attackerEntity)
+            {
+                DamageEvent damageEvent{targetEntity, weapon.Damage};
+
+                eventManager.Emit(damageEvent);
+
+                console.Log<Mosaic::Console::LogSeverity::Notice>("Entity {} attacks entity {} for {} damage", attackerEntity.ID, targetEntity.ID, weapon.Damage);
+            }
+        }
+    }
+}
+
+void TimeSystem(Mosaic::Resources& resources)
+{
+    auto& ecsManager = resources.ECSManager;
+    auto& eventManager = resources.EventManager;
+    auto& console = resources.Console;
+
+    auto timeView = ecsManager.QueryView<TimeTracker>();
+
+    for (auto [entity, time] : timeView)
+    {
+        if (time.Steps > time.Max)
+        {
+            eventManager.Emit(Mosaic::ApplicationExitEvent(0));
+        }
+        else
+        {
+            console.Log<Mosaic::Console::LogSeverity::Notice>("Iteration number: {}", time.Steps);
+
+            time.Steps++;
+        }
+    }
 }
 
 class MosaicTest : public Mosaic::Application
@@ -31,18 +118,29 @@ public:
     {
         auto& resources = GetApplicationResources();
 
-        auto& console = resources.Console;
         auto& ecsManager = resources.ECSManager;
+        auto& eventManager = resources.EventManager;
 
-        ecsManager.AddSystem(System);
+        ecsManager.AddSystem(AttackSystem);
+        ecsManager.AddSystem(TimeSystem);
 
-        Mosaic::ECS::Entity entity0 = ecsManager.CreateEntity();
-        Mosaic::ECS::Entity entity1 = ecsManager.CreateEntity();
+        eventManager.AddResponder<DamageEvent, Health>(OnDamage);
 
-        ecsManager.AddComponent<Component>(entity0, Component{.a = 7, .b = 9});
-        ecsManager.AddComponent<Component>(entity1, Component{.a = 3, .b = 2});
+        Mosaic::Entity entity0 = ecsManager.CreateEntity();
+        Mosaic::Entity entity1 = ecsManager.CreateEntity();
 
-        console.Log<Mosaic::Debug::Console::LogSeverity::Success>("Hello from Mosaic!");
+        ecsManager.AddComponent<Health>(entity0, Health(100));
+        ecsManager.AddComponent<Health>(entity1, Health(200));
+
+        ecsManager.AddComponent<Speed>(entity0, Speed(100));
+        ecsManager.AddComponent<Speed>(entity1, Speed(150));
+
+        ecsManager.AddComponent<Position>(entity0, Position(100, 200));
+        ecsManager.AddComponent<Position>(entity1, Position(200, 100));
+
+        ecsManager.AddComponent<Weapon>(entity1, Weapon(5));
+
+        ecsManager.AddComponent<TimeTracker>(entity0, TimeTracker(0, 10));
     }
 };
 
