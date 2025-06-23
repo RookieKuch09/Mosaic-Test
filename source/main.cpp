82f2cc1633acd970f2b6c3327b11e72028f4e251
RookieKuch09 <rookiekuch09@gmail.com>
2025-06-23 10:38:26 +0930

    API tweaks & Renderer showcase

--- source/main.cpp
+++ source/main.cpp
@@ -1,15 +1,44 @@
 #include <Mosaic/Mosaic.hpp>
 
+struct Time
+{
+    float Value;
+};
+
+void System(Mosaic::InstanceResources& instanceResources)
+{
+    auto& eventManager = instanceResources.EventManager;
+    auto& ecs = instanceResources.ECSManager;
+
+    auto view = ecs.QueryView<Time>();
+
+    glm::fvec4 colour;
+
+    for (auto [entity, component] : view)
+    {
+        component.Value += 0.001;
+
+        colour.r = -std::sin(component.Value);
+        colour.g = std::cos(component.Value);
+        colour.b = std::sin(component.Value);
+        colour.a = 1.0;
+    }
+
+    eventManager.Emit<Mosaic::RendererClearColourChangeRequest>(colour);
+}
+
 class MosaicTest : public Mosaic::Application
 {
 public:
-    MosaicTest(Mosaic::InstanceResources& resources)
-        : Application(resources)
+    void Setup(Mosaic::InstanceResources& instanceResources) override
     {
-    }
+        auto& ecs = instanceResources.ECSManager;
 
-    void Setup() override
-    {
+        auto entity = ecs.CreateEntity();
+
+        ecs.AddSystem(System);
+
+        ecs.AddComponent<Time>(entity);
     }
 };
 
